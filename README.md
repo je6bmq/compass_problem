ac3f2f2f7e058f7e36316d2d874cf7ac09f9ec18
je6bmq <je6bmq@gmail.com>
2016-11-21 01:15:12 +0900

    Kotlin版を実装。

--- README.md
+++ README.md
@@ -19,3 +19,21 @@ main関数内のanalysisというクロージャで解（理論式）を定義�
 
 # 検証
 現在、θ=π/3,2π/3,πの場合の検証は行っています。
+
+再帰関数の最適化とスタックの関係で、Kotlin版の方が高い精度で検証可能なようですが、Rust版も十分な精度で検証可能かと思われます。
+## Rust版
+Rust版は、
+
+```
+cargo run
+```
+
+で実行可能です。
+
+## Kotlin版
+Kotlin版は、
+
+```
+kotlinc Main.kt -include-runtime -d Main.jar
+kotlin -classpath Main.jar MainKt
+```
---
+++ kotlin/Main.kt
@@ -0,0 +1,35 @@
+import java.lang.Math;
+object MainKt {
+    tailrec fun compass(l:Double,m:Double,theta:Double,n:Double,o:Double,phi:Double=0.0):Double
+    {
+        return if(m<=0)
+        {
+            phi
+        }
+        else
+        {
+            val delta_phi=2.0*Math.PI/10000000
+            val ratio=o/n
+            val radius=Math.sqrt(Math.pow(l,2.0)+Math.pow(m,2.0)-2.0*l*m*Math.cos(theta))
+            val exhaust=radius*delta_phi*ratio
+
+            compass(l,m-exhaust,theta,n,o,phi+delta_phi)
+        }
+    }
+  @JvmStatic fun main(args:Array<String>) {
+      val n=0.001
+      val o=0.001
+      val theta=2.0*Math.PI/3
+      val l=1.0
+      val m0=1.0
+
+      val analysis={
+          needle:Double,carbon:Double,rad:Double,unitlen:Double,unitexh:Double->
+          val r0=Math.sqrt(Math.pow(needle,2.0)+Math.pow(carbon,2.0)-2.0*needle*carbon*Math.cos(rad))
+          (unitlen/unitexh)*Math.log(needle*(1.0+Math.cos(rad))/(needle*(Math.cos(rad)-carbon+r0)))
+          }
+
+      println("analysis:${analysis(l,m0,theta,n,o)}")    
+      println("simulation:${compass(l,m0,theta,n,o)}")
+  }
+}
